b8d4ca898ef538fd887c1217499bdc35b82ef66a
jihye yu <dbwlgp1yng@gmail.com>
2023-08-07 15:11:39 +0900

    feat: todolist 수정기능 변경

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,34 +1,39 @@
-import React, { useId, useState } from "react";
+import React, { useState } from "react";
 
 export default function TodoItem({ todo, onUpdate, onDelete }) {
   const { text } = todo;
-  const [isEditing, setIsEditing] = useState(false); 
+  const [isEditing, setIsEditing] = useState(false);
   const [updatedText, setUpdatedText] = useState(text);
-  const itemId = useId();
 
-  const handleChange = (e) => {
-    onUpdate({ ...todo });
-  };
-  const handleEditClick = () => {
-    setIsEditing(true);
-    setUpdatedText(text); 
-  };
   const handleUpdate = () => {
     onUpdate({ ...todo, text: updatedText });
     setIsEditing(false);
     setUpdatedText(updatedText);
   };
+  const handleCancel = () => {
+    setIsEditing(false);
+    setUpdatedText(text);
+  };
+  const handleToggleEdit = () => {
+    setIsEditing(!isEditing);
+    setUpdatedText(text);
+  };
   const handleDelete = () => onDelete(todo);
 
   return (
     <li className="flex justify-between py-2">
-      <label className="">
-        <input className="w-4 h-4" type="checkbox" id={`${itemId}`} onChange={handleChange} />
+      <label>
+        <input
+          className="w-4 h-4"
+          type="checkbox"
+          onChange={() => onUpdate({ ...todo })}
+        />
         <span className="pl-4">
-          {isEditing ? (
+          {isEditing ? (  
             <input
               className="pr-16"
               type="text"
+              data-testid="modify-input"
               value={updatedText}
               onChange={(e) => setUpdatedText(e.target.value)}
             />
@@ -38,16 +43,25 @@ export default function TodoItem({ todo, onUpdate, onDelete }) {
         </span>
       </label>
       <div>
-        <button 
-          className="mr-2"
-          data-testid="modify-button" 
-          onClick={isEditing ? handleUpdate : handleEditClick}
-        >
-          {isEditing ? "제출" : "수정"}
-        </button>
-        <button data-testid="delete-button" onClick={handleDelete}>
-          삭제
-        </button>
+        {isEditing ? (
+          <>
+            <button className="mr-2" data-testid="submit-button" onClick={handleUpdate}>
+              제출
+            </button>
+            <button data-testid="cancel-button" onClick={handleCancel}>
+              취소
+            </button>
+          </>
+        ) : (
+          <>
+            <button className="mr-2" data-testid="modify-button" onClick={handleToggleEdit}>
+              수정
+            </button>
+            <button data-testid="delete-button" onClick={handleDelete}>
+              삭제
+            </button>
+          </>
+        )}
       </div>
     </li>
   );
