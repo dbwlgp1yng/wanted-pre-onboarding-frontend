60bb0c35bcca5643154a52e0e5827189af864b0d
jihye yu <dbwlgp1yng@gmail.com>
2023-08-09 23:26:14 +0900

    feat: todo API 추가

---
+++ src/utils/TodoAPI.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+
+const API_BASE_URL = "https://www.pre-onboarding-selection-task.shop";
+
+export const createTodo = async (accessToken, todo) => {
+  const headers = {
+    Authorization: `Bearer ${accessToken}`,
+    "Content-Type": "application/json",
+  };
+  const response = await axios.post(
+    `${API_BASE_URL}/todos`,
+    { todo: todo },
+    { headers: headers },
+  );
+  return response.data;
+};
+
+export const getTodos = async (accessToken) => {
+  const headers = {
+    Authorization: `Bearer ${accessToken}`,
+  };
+  const response = await axios.get(
+    `${API_BASE_URL}/todos`,
+    { headers: headers },
+  );
+  return response.data;
+};
+
+export const updateTodo = async (accessToken, id, todoData) => {
+  const headers = {
+    Authorization: `Bearer ${accessToken}`,
+    "Content-Type": "application/json",
+  };
+  const response = await axios.put(
+    `${API_BASE_URL}/todos/${id}`,
+    todoData,
+    { headers: headers },
+  );
+  return response.data;
+};
+
+export const deleteTodo = async (accessToken, id) => {
+  const headers = {
+    Authorization: `Bearer ${accessToken}`,
+  };
+  await axios.delete(
+    `${API_BASE_URL}/todos/${id}`,
+    {}, 
+    { headers: headers }
+  );
+};
