89342c1ba52e216ec943ed0280d16569d1593bda
jihye yu <dbwlgp1yng@gmail.com>
2023-08-24 17:23:46 +0900

    refactor: Routing 로직 수정

--- src/App.js
+++ src/App.js
@@ -5,50 +5,18 @@ import Todo from './pages/Todo';
 
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
-
 function App() {
-  const handleRootPath = (path) => {
-    const isToken = localStorage.getItem('accessToken') === null ? false : true;
-    switch(path) {
-      case '/': 
-        if(isToken) {
-          return <Navigate to='/todo'></Navigate>
-        } else {
-          return <Navigate to='/signin'></Navigate>
-        }
-      case '/signin': 
-        if(isToken) {
-          return <Navigate to='/todo'></Navigate>
-        } else {
-          return <SignIn />
-        }
-      case '/signup': 
-        if(isToken) {
-          return <Navigate to='/todo'></Navigate>
-        } else {
-          return <SignUp />
-        }
-      case '/todo': 
-        if(isToken) {
-          return <Todo />
-        } else {
-          return <Navigate to='/signin'></Navigate>
-        }
-      default :
-        console.log('path가 없습니다.');
-    }
-  }
+  const isToken = localStorage.getItem('accessToken') !== null;
+
   return (
-    <>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <Routes>
-          <Route path='/' element={handleRootPath("/")} />
-          <Route path='/signin' element={handleRootPath("/signin")} />
-          <Route path='/signup' element={handleRootPath("/signup")} />
-          <Route path='/todo' element={handleRootPath("/todo")} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={isToken ? <Navigate to="/todo" /> : <Navigate to="/signin" />} />
+        <Route path="/signin" element={isToken ? <Navigate to="/todo" /> : <SignIn />} />
+        <Route path="/signup" element={isToken ? <Navigate to="/todo" /> : <SignUp />} />
+        <Route path="/todo" element={isToken ? <Todo /> : <Navigate to="/signin" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
